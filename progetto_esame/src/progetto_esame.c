ab6c22b4d11ad0397a685b9b9ea2af77c14be892
sergio <sergio@DESKTOP-M6L6E0D.mshome.net>
2018-08-28 18:07:22 +0200

    programma sulle temperature dei mesi che il prof dette all`esame

--- progetto_esame/src/progetto_esame.c
+++ progetto_esame/src/progetto_esame.c
@@ -11,8 +11,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+
+	char mese[21];
+	int temperatura;
+
+}vettore_temperature;
+
+void sposta_contenuto_file(FILE *fp,vettore_temperature temp[]);
+float media_temperatura(vettore_temperature temp[]);
+int temperatura_piu_alta(vettore_temperature temp[]);
+void stampa_temperatura_alta(vettore_temperature temp[]);
+
+
 int main(void) {
-	printf("ciao");
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	FILE *fp;
+	vettore_temperature temp[20];
+	float media;
+
+
+	fp=fopen("vet_temp.txt","r");
+
+	if(fp==NULL){
+		printf("errore nell`apertura del file");
+	}else{
+
+		sposta_contenuto_file(fp,temp);
+
+		printf("\necco la media delle temperature\n");
+		media=media_temperatura(temp);
+		printf("%1.2f\n",media);
+
+		stampa_temperatura_alta(temp);
+
+	}
+
+	system("pause");
 	return EXIT_SUCCESS;
+}
+
+void sposta_contenuto_file(FILE *fp,vettore_temperature temp[]){
+
+	int i;
+	char carattere;
+
+	i=0;
+	while((carattere=fgetc(fp))!=EOF){
+		if(carattere=='('){
+			fscanf(fp,"%s%d*c",temp[i].mese,&temp[i].temperatura);
+			i=i+1;
+		}
+
+	}
+}
+
+float media_temperatura(vettore_temperature temp[]){
+
+	int i;
+	int somma;
+	float media;
+
+	somma=0;
+	i=0;
+	while(i<12){
+		somma=somma+temp[i].temperatura;
+		i=i+1;
+	}
+	media=(float)somma/12;
+	return media;
+
+}
+
+int temperatura_piu_alta(vettore_temperature temp[]){
+
+	int i;
+	int temp_alta;
+
+	temp_alta=0;
+	i=0;
+	while(i<12){
+		if(temp[i].temperatura>temp_alta){
+			temp_alta=temp[i].temperatura;
+		}
+		i=i+1;
+	}
+	return temp_alta;
+}
+
+void stampa_temperatura_alta(vettore_temperature temp[]){
+
+	int i;
+	int temperatura_alta;
+
+	temperatura_alta=temperatura_piu_alta(temp);
+	i=0;
+	while(i<12){
+		if(temp[i].temperatura==temperatura_alta){
+			printf("(%s) ",temp[i].mese);
+		}
+		i=i+1;
+	}
 }
